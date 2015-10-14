0165b9bfba1e2d07ba89bdbcad45e6562faf09e3
SilverDragoon <silvir007@gmail.com>
2015-10-15 00:29:36 +0300

    Реализована валидация в форме обратной связи

--- app/js/feedback.js
+++ app/js/feedback.js
@@ -1,3 +1,40 @@
-$(document).ready(function() {
-	jQuery('input[placeholder], textarea[placeholder]').placeholder();
-});
+var contactMe = (function() {
+
+	//Инициализируем модуль
+	var init = function() {
+		_setUpListners();
+	};
+
+	//Прослушиваем события
+	var _setUpListners = function() {
+		$("#feedback-form").on('submit', _submitForm);
+	};
+
+	//Отправка формы
+	var _submitForm = function(event){
+		console.log('Отправка формы')
+		event.preventDefault();
+
+		var form = $(this),
+			url = 'feedback.php',
+			defObj = _ajaxForm(form, url);
+
+			//Действие с ответом с сервера defObj
+	};
+
+	var _ajaxForm = function(form, url) {
+		console.log('Ajax запрос')
+		if (!validation.validateForm(form)) return false;
+		// Если false, то код ниже не выполнится
+
+	};
+
+	return {
+		init: init
+	}
+})();
+
+contactMe.init();
+
+jQuery('input[placeholder], textarea[placeholder]').placeholder();
+
---
+++ app/js/validation.js
@@ -0,0 +1,97 @@
+var validation = (function () {
+
+	// Инициализируем наш модуль
+	var init = function () {
+		_setUpListners();
+	};
+
+	// Прослушиваем события 
+	var _setUpListners = function () {
+		$('form').on('keydown', '.input-error', _removeError);
+		$('form').on('reset', _clearForm);
+	};
+
+	var _removeError = function () {
+		$(this).removeClass('input-error');
+	};
+
+	var _clearForm = function (form) {
+		var form = $(this);
+		form.find('input, textarea').trigger('hideTooltip');
+		form.find('.input-error').removeClass('input-error')
+	};
+
+	//Создает qTip
+	var _createQtip = function (element, position) {
+
+		//Позиция qTip
+		if (position === 'rigth') {
+			position = {
+				my: 'left center',
+				at: 'rigth center'
+			}
+		} else {
+			position = {
+				my: 'rigth center',
+				at: 'left center',
+				adjust: {
+					method: 'shift none'
+				}
+			}
+		};
+
+		// Инициализируем qTip
+		element.qtip({
+			content : {
+				text: function() {
+					return $(this).attr('qtip-content');
+				}
+			},
+			show: {
+				event: 'show'
+			},
+			hide: {
+				event: 'keydown hideTooltip'
+			},
+			position: position,
+			style: {
+				classes: 'qtip-mystyle qtip-rounded',
+				tip: {
+					height: 10,
+					width: 16
+				}
+			}
+		}).trigger('show');
+	};
+
+	//Валидация формы (Универсальная функция)
+	var validateForm = function (form) {
+
+		console.log('Валидация');
+		var elements = form.find('input, textarea').not('input[type="file"], input[type="hidden"]'),
+			valid = true;
+
+		//Пройдемся по всем элементам формы
+		$.each(elements, function (index, val){
+			var element = $(val),
+				val = element.val();
+				pos = element.attr('qtip-position');
+
+			if (val.length === 0) {
+				element.addClass('input-error');
+				_createQtip(element, pos);
+				valid = false;
+			}
+		});
+
+		return valid;
+	};
+
+	//Возвращаем объект (Публичные методы)
+	return {
+		init: init,
+		validateForm: validateForm
+	}
+})();
+
+validation.init();
