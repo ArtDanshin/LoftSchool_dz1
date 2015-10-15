f906fbff5d665c1554e36d5498ebe2539ff0f9a1
SilverDragoon <silvir007@gmail.com>
2015-10-15 12:57:13 +0300

    Сделаны тултипы в форме авторизации

--- app/js/auth.js
+++ app/js/auth.js
@@ -1,3 +1,37 @@
-$(document).ready(function() {
-	jQuery('input[placeholder], textarea[placeholder]').placeholder();
-});
+var аuth = (function() {
+
+	//Инициализируем модуль
+	var init = function() {
+		_setUpListners();
+	};
+
+	//Прослушиваем события
+	var _setUpListners = function() {
+		$("#authform").on('submit', _submitForm);
+	};
+
+	//Отправка формы
+	var _submitForm = function(event){
+		event.preventDefault();
+
+		var form = $(this),
+			url = 'auth.php',
+			defObj = _ajaxForm(form, url);
+
+			//Действие с ответом с сервера defObj
+	};
+
+	var _ajaxForm = function(form, url) {
+		if (!validation.validateForm(form)) return false;
+		// Если false, то код ниже не выполнится
+
+	};
+
+	return {
+		init: init
+	}
+})();
+
+аuth.init();
+
+jQuery('input[placeholder], textarea[placeholder]').placeholder();
