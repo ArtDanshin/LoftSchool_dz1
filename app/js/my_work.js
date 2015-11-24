abecf238aade8a3739a7fa2969658aac3b6ba243
SilverDragoon <silvir007@gmail.com>
2015-11-24 15:22:36 +0300

    Переформирование проекта для добавления серверной стороны

--- app/js/my_work.js
+++ app/js/my_work.js
@@ -45,45 +45,27 @@ var myModule = (function () {
 		event.preventDefault();
 
 		var form = $(this),
-			url = 'add_project.php',
-			defObj = _ajaxForm(form,url)
-
-		//Если JS валидация успешна, то Ajax запрос на сервер
-		if (defObj) {
-			defObj.done(function(ans) {
-				if (ans.status ==='OK') {
-					console.log(ans.text);
-				} else {
-					console.log(ans.text);
-				}
-			})
-		}
-	};
-
-//Универсальня функция
-// Для ее работы используется
-// @form - форма
-// @url - адрес php файла к которому мы обращаемся
-// 1. Собирает данные из формы
-//2. Проверяет форму
-// 3. Делает запрос на сервер и возвращает ответ с сервера
-var _ajaxForm = function (form, url) {
+			url = 'php/add_project.php',
+			data = form.serialize();
 
 		if (!validation.validateForm(form)) return false;
 
-		data = form.serialize();
-
-		var result = $.ajax({
-				url: url,
-				type: 'POST',
-				dataType: 'json',
-				data: data
-				})
-				.fail(function(ans) {
-					console.log('Проблемы в PHP');
-				});
-
-		return result;
+		$.ajax({
+			url: url,
+			type: 'POST',
+			dataType: 'json',
+			data: data,
+		})
+		.done(function(ans) {
+			if (ans.status ==='OK') {
+				console.log(ans.text);
+			} else {
+				console.log(ans.text);
+			}
+		})
+		.fail(function(ans) {
+			console.log('Проблемы в PHP');
+		})
 	};
 
 //Возвращаем значения
@@ -98,4 +80,5 @@ if (typeof console === "undefined" || typeof console.log === "undefined") {
 	};
 	
 myModule.init();
+
 jQuery('input[placeholder], textarea[placeholder]').placeholder();
